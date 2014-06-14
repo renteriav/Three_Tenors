01d6890c6246760c51f23f2c9be6859329addf90
Francisco Renteria <renteriav@hotmail.com>
2014-06-14 13:40:10 -0700

    Changed after destroy

--- app/models/sound_clip.rb
+++ app/models/sound_clip.rb
@@ -13,15 +13,22 @@
 class SoundClip < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader
   
-  after_destroy :remove_id_directory
+  #after_destroy :remove_id_directory
+  after_destroy :remove_file_directory
     
   validates :title, presence: true
   validates :attachment, presence: true
 
   protected
+
+
+   def remove_file_directory
+      path = File.expand_path(attachment.store_path, attachment.root)
+      FileUtils.remove_dir(path, force: false)
+   end
   
-  def remove_id_directory
-   FileUtils.remove_dir("#{Rails.root}/public/uploads/sound_clip/attachment/#{id}", :force => true)
-  end
+ # def remove_id_directory
+ #  FileUtils.remove_dir("#{Rails.root}/public/uploads/sound_clip/attachment/#{id}", :force => true)
+ # end
 
 end
